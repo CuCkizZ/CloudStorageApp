959b45c72cef71311dd5ad3c5438be5264116a9c
CuCkizZ <CuCki327@gmail.com>
2024-07-11 13:11:39 +0300

    added new custom colors

---
+++ CloudStarageApp/CloudStarageApp/Resourses/AppColors.swift
@@ -0,0 +1,14 @@
+//
+//  AppColors.swift
+//  CloudStarageApp
+//
+//  Created by Nikita Beglov on 11.07.2024.
+//
+
+import UIKit
+
+enum AppColors {
+    static let standartBlue = UIColor(hex: 0x383FF5)
+    static let storagePink = UIColor(hex: 0xF1AFAB)
+    static let customGray = UIColor(hex: 0x9E9E9E)
+}
--- CloudStarageApp/CloudStarageApp/ViewController.swift
+++ CloudStarageApp/CloudStarageApp/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = AppColors.storagePink
         testSetup()
     }
 
