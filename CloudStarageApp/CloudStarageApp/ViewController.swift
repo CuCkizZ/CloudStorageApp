be13e55be461bb24a9596d9e31acaf2f72952a2a
CuCkizZ <CuCki327@gmail.com>
2024-07-11 12:02:31 +0300

    added project, deleter storyboard

---
+++ CloudStarageApp/CloudStarageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudStarageApp
+//
+//  Created by Nikita Beglov on 11.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
