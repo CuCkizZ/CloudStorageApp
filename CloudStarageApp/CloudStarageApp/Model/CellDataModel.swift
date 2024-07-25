cfaf89d9626f91f3b11ff18a4ee9c773c8942ef6
CuCkizZ <CuCki327@gmail.com>
2024-07-25 12:40:43 +0300

    removed UILabel(title) and setup largeTitle

--- CloudStarageApp/CloudStarageApp/Model/CellDataModel.swift
+++ CloudStarageApp/CloudStarageApp/Model/CellDataModel.swift
@@ -25,6 +25,21 @@ struct MappedDataModel {
             Files(name: "File 3", size: "4gb", date: "42.42.2022", icon: #imageLiteral(resourceName: "file")),
             Files(name: "File 4", size: "4gb", date: "51.12.2020", icon: #imageLiteral(resourceName: "file")),
             Files(name: "File 5", size: "5gb", date: "01.13.2013", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "Dwadsdjas", size: "2gb", date: "12.13.2022", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 2", size: "5gb", date: "12.13.2021", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 3", size: "4gb", date: "42.42.2022", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 4", size: "4gb", date: "51.12.2020", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 5", size: "5gb", date: "01.13.2013", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 1", size: "2gb", date: "12.13.2022", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 2", size: "5gb", date: "12.13.2021", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 3", size: "4gb", date: "42.42.2022", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 4", size: "4gb", date: "51.12.2020", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 5", size: "5gb", date: "01.13.2013", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 1", size: "2gb", date: "12.13.2022", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 2", size: "5gb", date: "12.13.2021", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 3", size: "4gb", date: "42.42.2022", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 4", size: "4gb", date: "51.12.2020", icon: #imageLiteral(resourceName: "file")),
+            Files(name: "File 5", size: "5gb", date: "01.13.2013", icon: #imageLiteral(resourceName: "file"))
         ]
     }
 }
